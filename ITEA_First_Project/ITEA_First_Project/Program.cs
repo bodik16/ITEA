ba144b398dfdcaac4c6e3df470ea0d3dbc32f342
Bodik_16 <bogdanshomko@gmail.com>
2019-05-06 20:53:55 +0300

    iNITIAL COMMIT

---
+++ ITEA_First_Project/ITEA_First_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITEA_First_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
