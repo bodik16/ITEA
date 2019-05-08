7763ccbd906f57a8e00b553f894dc1e9625a9d80
Bodik_16 <bogdanshomko@gmail.com>
2019-05-08 20:47:19 +0300

    I`ve done second task. Lesson 2

---
+++ Lesson2_Task2/Lesson2_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i <= 255)
+            {
+                Console.WriteLine(i + " = " + (char)i);
+
+                i++;
+            }
+            //for (int i = 0; i < 255; i++)
+            //{
+            //    Console.WriteLine($"number: [{i}] = [{(char)i}]");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
