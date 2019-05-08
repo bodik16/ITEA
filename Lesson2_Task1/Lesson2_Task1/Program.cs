39c218c3d0197ddbc251300194e2c2fa3a171d68
Bodik_16 <bogdanshomko@gmail.com>
2019-05-08 20:44:07 +0300

    I`ve done first task. Lesson2

---
+++ Lesson2_Task1/Lesson2_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson2_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello, my dear friend";
+        
+            int countWords = word.Split().Length;
+            Console.WriteLine($"Count: {countWords}");
+            Console.ReadKey();
+
+
+            string ourText = File.ReadAllText("C:\\Users\\Bogda\\Documents\\ITEA\\OurText.txt");
+            int countWordsFromTXT = ourText.Split().Length;
+            Console.WriteLine($"Count from .txt: {countWordsFromTXT}");
+            Console.ReadKey();
+
+        }
+
+    }
+}
