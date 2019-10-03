986f705ee8aa673628cd25778e93d9712ff737a7
Bodik_16 <bogdanshomko@gmail.com>
2019-10-03 11:34:17 +0300

    FirstSubject

---
+++ FirstProjectIteaAdvanced/FirstProjectIteaAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProjectIteaAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
